4e0c40d83bcbaf01c1fe025c1ca36dfe5315cdaa
VivianRMS <vivianrklds221228@gmail.com>
2023-11-13 01:34:42 -0500

    Initial Commit

---
+++ ScrumDinger/ScrumDingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumDingerApp.swift
+//  ScrumDinger
+//
+//  Created by Kunlin Ruan on 11/13/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumDingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
